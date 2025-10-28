92e9ee549df3cc3fba69d285a6c2c50f7fe224c5
Rahmadjon0038 <abdullayevrahmadjon821@gmail.com>
2025-10-28 11:02:19 +0500

    s

--- config/db.js
+++ config/db.js
@@ -1,15 +1,15 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'rahmadjon',
-  password: 'yangi_parolingiz',
-  database: 'ieltsMock',
+  host: "localhost",
+  user: "rahmadjon",
+  password: "SizningYangiParolingiz",
+  database: "ieltsMock",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
   enableKeepAlive: true,
-  keepAliveInitialDelay: 0
+  keepAliveInitialDelay: 0,
 });
 
 module.exports = pool;
