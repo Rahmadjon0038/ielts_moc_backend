82e5aac0e0657f20bd6da1ddbb83c073942b43f3
Murodillayev Hojiakbar <102051063+uzhojiakbar@users.noreply.github.com>
2025-12-19 22:59:22 +0500

    docker and psql fix

--- config/db.js
+++ config/db.js
@@ -2,23 +2,22 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  user: process.env.POSTGRES_USER || "rahmadjon",
-  host: process.env.POSTGRES_HOST || process.env.PGHOST || "0.0.0.0",
-  database: process.env.POSTGRES_DB || process.env.PGDATABASE || "ieltsmock",
-  password: process.env.POSTGRES_PASSWORD || process.env.PGPASSWORD || "ieltsmock12345",
-  port: Number(process.env.POSTGRES_PORT || process.env.PGPORT || 4010),
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST, // postgres15
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+  port: Number(process.env.POSTGRES_PORT), // 5432
   max: 10,
   idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
+  connectionTimeoutMillis: 5000,
 });
 
-// Ulanishni sinash (ixtiyoriy)
 pool.on("connect", () => {
-  console.log("PostgreSQL bazasi bilan muvaffaqiyatli ulandi.");
+  console.log("✅ PostgreSQL bazasi bilan muvaffaqiyatli ulandi");
 });
 
 pool.on("error", (err) => {
-  console.error("PostgreSQL ulanish poolida kutilmagan xato:", err);
+  console.error("❌ PostgreSQL pool xatosi:", err);
 });
 
 module.exports = pool;
